3f400768b88401e1e80ed834d4cc2fde2167c057
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-04-13 14:48:42 -0400

    React Hooks DQ Components

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
-
+/*Ordered list of elements
+1.App
+2.Side Menu>logo>Menu Links>Search Bar
+3.Top Menu>
+4.Main Content>Genre Picker> Album Cards>Album List*/
 ReactDOM.render(<App />, document.getElementById("root"));
+
